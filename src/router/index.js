a47048d7fedb8656f62b5b4320874fc495f91d0a
vakesamahere <vakegamer@163.com>
2024-08-06 12:25:27 +0800

    login-logo

--- src/router/index.js
+++ src/router/index.js
@@ -11,7 +11,8 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      // component: () => import('../views/AboutView.vue'),
+      component: () => import('../components/Help.vue'),
       meta: { 
         requiresAuth: true,
         index:4
