13ffa2f5ab23a9d388bd57ff886c0191fb17604e
calvin <2912749031@qq.com>
2024-08-08 16:03:23 +0800

    增加首页

--- src/router/index.js
+++ src/router/index.js
@@ -7,14 +7,14 @@ const router = createRouter({
   routes: [
     {
       path: '/',
-      name: 'about',
+      name: 'about1',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       // component: () => import('../views/AboutView.vue'),
       component: () => import('../components/Help.vue'),
       meta: { 
-        requiresAuth: true,
+        requiresAuth: false,
         index:4
       } 
     },
